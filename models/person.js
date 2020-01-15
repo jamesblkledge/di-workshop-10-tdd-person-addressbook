12626d2be89363fa911dfe803a9304d3bb878ee6
James Blackledge <james.blackledge@ada.ac.uk>
2020-01-15 12:58:03 +0000

    Finished Task 1.

--- models/person.js
+++ models/person.js
@@ -1,1 +1,59 @@
-// Your Person class goes here!
+class Person {
+    constructor(firstName, lastName, dateOfBirth) {
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1);
+        this.lastName = lastName.charAt(0).toUpperCase() + lastName.slice(1);
+        this.dateOfBirth = dateOfBirth;
+        this.emailAddresses = [];
+        this.phoneNumbers = [];
+    }
+
+    fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    addEmail() {
+        for (let i = 0; i < arguments.length; i++) {
+            this.emailAddresses.push(arguments[i]);
+        }
+
+        return false;
+    }
+
+    addPhoneNumber() {
+        for (let i = 0; i < arguments.length; i++) {
+            this.phoneNumbers.push(arguments[i]);
+        }
+
+        return false;
+    }
+
+    returnFormattedDetails() {
+        let dottedLine = "";
+
+        for (let i = 0; i < this.fullName().length; i++) {
+            dottedLine += "-";
+        }
+
+        let email = "";
+        this.emailAddresses.forEach((e) => {
+            email += "- " + e + "\n";
+        });
+
+        let phone = "";
+        this.phoneNumbers.forEach((e) => {
+            phone += "- " + e + "\n";
+        });
+
+        let lines = [
+            this.fullName(),
+            dottedLine,
+            "DOB: " + this.dateOfBirth + "\n\n",
+            "Email Addresse(s):\n" + email + "\n",
+            "Phone Number(s):\n" + phone
+        ];
+
+        return lines.join("\n");
+    }
+}
+
+module.exports = Person;
--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,74 @@
-const chai = require('chai')
-const expect = chai.expect
+const chai = require('chai');
+const expect = chai.expect;
 
-// Your Person test goes here!
+const Person = require('../models/person.js');
+
+describe('Person', () => {
+    //CONSTRUCTOR
+    describe('#constructor', () => {
+        it('should initialise properly', () => {
+            let person = new Person('Jeff', 'Bezos', '11 Jun 2001');
+    
+            expect(person.firstName).to.equal('Jeff');
+            expect(person.lastName).to.equal('Bezos');
+            expect(person.dateOfBirth).to.equal('11 Jun 2001');
+        });
+
+        it('should capitalise the name correctly', () => {
+            let person = new Person('jeff', 'bezos', '11 Jun 2001');
+
+            expect(person.firstName).to.equal('Jeff');
+            expect(person.lastName).to.equal('Bezos');
+        });
+    });
+
+
+    //FULLNAME
+    describe('#fullName()', () => {
+        it('should concatenate the name properly', () => {
+            let person = new Person('jeff', 'bezos', '11 Jun 2001');
+
+            expect(person.fullName()).to.equal('Jeff Bezos');
+        });
+    });
+
+
+    //ADDEMAIL
+    describe('#addEmail()', () => {
+        it('should add 1 email', () => {
+            let person = new Person('jeff', 'bezos', '11 Jun 2001');
+
+            person.addEmail("jeff@amazon.com");
+
+            expect(person.emailAddresses).to.deep.equal(["jeff@amazon.com"]);
+        });
+
+        it('should add multiple (variable amount) emails', () => {
+            let person = new Person('jeff', 'bezos', '11 Jun 2001');
+
+            person.addEmail("jeff@amazon.com", "mrbezos@gmail.com", "jeff.bezos@outlook.com", "bezosboy67@icloud.com");
+
+            expect(person.emailAddresses).to.deep.equal(["jeff@amazon.com", "mrbezos@gmail.com", "jeff.bezos@outlook.com", "bezosboy67@icloud.com"]);
+        });
+    });
+
+    
+    //ADDPHONENUMBER
+    describe('#addPhoneNumber()', () => {
+        it('should add 1 phone number', () => {
+            let person = new Person('jeff', 'bezos', '11 Jun 2001');
+
+            person.addPhoneNumber("07954678432");
+
+            expect(person.phoneNumbers).to.deep.equal(["07954678432"]);
+        });
+
+        it('should add multiple (variable amount) phone numbers', () => {
+            let person = new Person('jeff', 'bezos', '11 Jun 2001');
+
+            person.addPhoneNumber("07954678432", "0215423986", "0208456231", "07776214899");
+
+            expect(person.phoneNumbers).to.deep.equal(["07954678432", "0215423986", "0208456231", "07776214899"]);
+        });
+    });    
+});
